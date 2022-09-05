5fad620020ead3f69bca8ac668428bbdbacf3ce8
Addie Liu <liuaddie@gmail.com>
2022-09-05 22:18:53 +0800

    Update shapes_detector.jsx

--- expressions/shapes_detector.jsx
+++ expressions/shapes_detector.jsx
@@ -1,73 +1,63 @@
 {
-	"shapeInfoClass":function(path, step){
-		this.path = path;
-		this.step = step;
-		var pa = this.path;
-		var pp = this.path.points(0);
-		var ppSum = [0,0];
-		var step = this.step;
-		var tar = 0;
-		var tarOnPath = 0;
-		var minDist = 0;
-		var minDistNew = 0;
-		var ppMax = [0,0];
-		var ppMin = [0,0];
-		for (i=0; i<step; i++){
-			ppOnPath = pa.pointOnPath(i/step,0);
-			dist = length(pp[tar], ppOnPath);
-			if (i == 0){
-				minDist = dist;
-				ppSum = pa.pointOnPath(0,0);
-				ppMax = pa.pointOnPath(0,0);
-				ppMin = pa.pointOnPath(0,0);
-			}
-			ppSum += ppOnPath;
-			minDistNew = Math.min(minDist, dist);
-			ppMin = [Math.min(ppMin[0], ppOnPath[0]), Math.min(ppMin[1], ppOnPath[1])];
-			ppMax = [Math.max(ppMax[0], ppOnPath[0]), Math.max(ppMax[1], ppOnPath[1])];
-			if (minDistNew !== minDist){
-				minDist = minDistNew;
-				tarOnPath = i/step;
-			}
-		}
-		this.pos = pa.pointOnPath(tarOnPath,0);
-		this.ptCenter = ppSum/step;
-		this.ptMin = ppMin;
-		this.ptMax = ppMax;
-	},
-
-	"findPoint":function(TargetPath, TargetPoint){
+	"shapeObj":function(TargetPath, TargetTime,  TargetStep){
+		TargetTime = (typeof TargetTime !== 'undefined') ?  TargetTime : 0;
+		TargetStep = (typeof TargetStep !== 'undefined') ?  TargetStep : 60;
 		var pa = TargetPath;
-		var pp = pa.points(0);
+		var tt = TargetTime;
+		var step = TargetStep;
+		var pp = TargetPath.points(tt);
 		var ppSum = [0,0];
-		var step = 100;
-		var tar = TargetPoint;
-		var tarOnPath = 0;
-		var minDist = 0;
-		var minDistNew = 0;
 		var ppMax = [0,0];
 		var ppMin = [0,0];
-		for (i=0; i<step; i++){
-			ppOnPath = pa.pointOnPath(i/step,0);
-			dist = length(pp[tar], ppOnPath);
-			if (i == 0){
-				minDist = dist;
-				ppSum = pa.pointOnPath(0,0);
-				ppMax = pa.pointOnPath(0,0);
-				ppMin = pa.pointOnPath(0,0);
-			}
-			ppSum += ppOnPath;
-			minDistNew = Math.min(minDist, dist);
-			ppMin = [Math.min(ppMin[0], ppOnPath[0]), Math.min(ppMin[1], ppOnPath[1])];
-			ppMax = [Math.max(ppMax[0], ppOnPath[0]), Math.max(ppMax[1], ppOnPath[1])];
-			if (minDistNew !== minDist){
-				minDist = minDistNew;
-				tarOnPath = i/step;
-			}
-		}
-			// return pa.pointOnPath(tarOnPath,0);
-			// return ppSum/step;
-			return ppMax;
+		var pt = []; // all points
+		var ptOnPath = []; // pointOnPath percentage, accuracy depends on steps
+		var ptDist = []; // internal use
+		var ptMinDist = []; // internal use
+		var ptMinDistNew = []; // internal use
+
+		//intial pointOnPath percentage of all points to zero
+		for (k=0; k<pp.length; k++){
+			pt.push(pa.points(tt)[k]);
+			ptOnPath.push(0);
+			ptDist.push(0);
+			ptMinDist.push(0);
+			ptMinDistNew.push(0);
 		}
 
-}
+		// check following items for each steps:
+		// pointOnPath Percentage, Real Shape Center, Shape Min & Max points
+		for (i=0; i<step; i++){
+			ppOnPath = pa.pointOnPath(i/step,tt);
+
+			// pointOnPath percentage
+			for (j=0; j<pp.length; j++){
+				ptDist[j] = length(pt[j], ppOnPath);
+				ptMinDist[j] = (i==0) ? ptDist[j] : ptMinDist[j];
+				ptMinDistNew[j] = Math.min(ptMinDist[j], ptDist[j]);
+				if (ptMinDistNew[j] !== ptMinDist[j]){
+					ptMinDist[j] = ptMinDistNew[j];
+					ptOnPath[j] = i/step;
+				}
+			}
+
+			// Center, Min & Max
+			if (i == 0){
+				ppSum = pa.pointOnPath(0,tt);
+				ppMax = pa.pointOnPath(0,tt);
+				ppMin = pa.pointOnPath(0,tt);
+			}
+			ppSum += ppOnPath;
+			ppMin = [Math.min(ppMin[0], ppOnPath[0]), Math.min(ppMin[1], ppOnPath[1])];
+			ppMax = [Math.max(ppMax[0], ppOnPath[0]), Math.max(ppMax[1], ppOnPath[1])];
+		}
+
+		// Return for object properties
+		this.shape = TargetPath.valueAtTime(TargetTime); // Shape of TargetTime
+		this.pt = pt; // Array, All points
+		this.ptOnPath = ptOnPath; // Array, pointOnPath Percentage of all points
+		this.ptCenter = ppSum/step; // Position, Real Shape Center
+		this.ptMin = ppMin; // Position, Shape Min Point
+		this.ptMax = ppMax; // Position, Shape Max Point
+	} // End of shapeObj
+
+} // End of JSON
